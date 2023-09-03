f8f662586462dd0ba5f610e8d93d8f5ee46bdf5d
Luan Rafael <luanraffaell@hotmail.com>
2023-09-03 20:47:41 -0300

    feat: create UserController

---
+++ src/main/java/com/picpaysimplificado2/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.picpaysimplificado2.controllers;
+
+import com.picpaysimplificado2.dtos.UserDTO;
+import com.picpaysimplificado2.services.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private UserService userService;
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO user){
+        UserDTO newUser = this.userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+        UserDTO user = this.userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+}
