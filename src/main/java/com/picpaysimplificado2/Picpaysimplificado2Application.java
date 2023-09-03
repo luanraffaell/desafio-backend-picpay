8183d20261d65d8ef185e217616b4e996a97fa6c
Luan Rafael <luanraffaell@hotmail.com>
2023-09-03 20:40:14 -0300

    feat: initial config

---
+++ src/main/java/com/picpaysimplificado2/Picpaysimplificado2Application.java
@@ -0,0 +1,13 @@
+package com.picpaysimplificado2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Picpaysimplificado2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Picpaysimplificado2Application.class, args);
+	}
+
+}
