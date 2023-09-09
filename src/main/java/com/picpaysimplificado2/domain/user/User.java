5cbab36e1cab5554a08d94d14ef2d5e0ff9005c6
Luan Rafael <luanraffaell@hotmail.com>
2023-09-09 10:25:24 -0300

    feat: implements userDetails

--- src/main/java/com/picpaysimplificado2/domain/user/User.java
+++ src/main/java/com/picpaysimplificado2/domain/user/User.java
@@ -2,9 +2,14 @@ package com.picpaysimplificado2.domain.user;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,7 +18,7 @@ import java.math.BigDecimal;
 @EqualsAndHashCode(of = "id")
 @Entity
 @Table(name = "users")
-public class User implements Serializable {
+public class User implements Serializable, UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,4 +31,39 @@ public class User implements Serializable {
     private String password;
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(userType.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
